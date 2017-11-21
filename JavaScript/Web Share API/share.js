58b63808e872773f04a0986ca1c54981f26ab06f
Mazso <thomas.steinberg@elbwiese.de>
2017-11-21 09:32:21 +0100

    aufgeräumt und kommentiert

--- JavaScript/Web Share API/share.js
+++ JavaScript/Web Share API/share.js
@@ -1,25 +1,28 @@
 (function() {
-       
+    // beherrscht Brwoser Web Share API?
     if (navigator.share !== undefined) {
-            var shareApi = document.getElementById('teilen');
-            var shareButton = document.createElement("button");
-            shareButton.setAttribute("id", "share");
-            var shareSpan = document.createElement("span");       
-            shareApi.appendChild(shareButton);
-            var share = document.getElementById('share');
-            share.appendChild(shareSpan);
-            shareSpan.textContent = "Teilen!";
-            shareSpan.classList.add("vs");
-
-    var shareUrl = document.querySelector('link[rel=canonical]') ? document.querySelector('link[rel=canonical]').href : window.location.href;
-    
-    share.addEventListener('click', function (ev) {
-
-    
-        navigator.share({
-            title: document.title,
-            url: shareUrl
-        });
-	});
-}
+        // finde div, in das der Button eingebaut werden soll
+        var shareApi = document.getElementById('teilen');
+        // Button konstruieren
+        var shareButton = document.createElement("button");
+        shareButton.setAttribute("id", "share");
+        // span konstruieren mit Text
+        var shareSpan = document.createElement("span");       
+        shareApi.appendChild(shareButton);
+        var share = document.getElementById('share');
+        share.appendChild(shareSpan);
+        shareSpan.textContent = "Teilen!";
+        // class dient zum optischen Ausblenden des Texts über CSS (postion:absolute), Button wird grafisch gestaltet
+        shareSpan.classList.add("vs");
+        
+        // Web Share API aufrufen
+        share.addEventListener('click', function (ev) {
+            // sucht Kanonische Link-Angabe, fehlt die, wird die URI aus der Adresszeile übernommen
+            var shareUrl = document.querySelector('link[rel=canonical]') ? document.querySelector('link[rel=canonical]').href : window.location.href;
+                navigator.share({
+                    title: document.title,
+                    url: shareUrl
+                });
+            });
+        }
 })();
