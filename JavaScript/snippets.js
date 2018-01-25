0568b0c980f1de8b06d9abfa5b2d1018bc42b09b
Mazso <thomas.steinberg@elbwiese.de>
2018-01-25 17:55:37 +0100

    Schleifen mit slice; touch

--- JavaScript/snippets.js
+++ JavaScript/snippets.js
@@ -1,3 +1,18 @@
 [].slice.call(document.getElementsByClassName('myClass')).forEach(function(element) {
     element.classList.remove('mySecondCalls');
 });
+
+
+
+   // Register touchstart and touchend listeners for element 'source'
+    var src = document.getElementById("main");
+    src.addEventListener('touchmove', function (a) {
+        [].slice.call(document.getElementsByClassName('liste')).forEach(function(element) {
+                element.classList.add('scroll');
+            }); 
+           setTimeout(function(){ 
+                [].slice.call(document.getElementsByClassName('liste')).forEach(function(element) {
+                element.classList.remove('scroll');
+            }); 
+            }, 500);
+    }, false);
